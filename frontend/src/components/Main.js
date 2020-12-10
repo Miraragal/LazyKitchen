e5d763aff22380077d9ac7fe6a15c3f6b36f05c6
Miriam <miraragal@gmail.com>
2020-12-09 17:23:11 -0700

    LandingPage

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Switch, Route, Redirect, withRouter } from "react-router-dom";
+
+
+class Main extends React.Component{
+render(){
+    return(
+    <div className="container-fluid">
+        <img src="/images/spices.jpg" alt="Landing" />
+    </div>
+    )
+}
+}
+
+export default Main;
