ccce13b6eb8d75886f5202abe194c5489d4d0b8a
Miriam <miraragal@gmail.com>
2020-12-10 12:21:02 -0700

    Explore form

--- frontend/src/components/Explore.js
+++ frontend/src/components/Explore.js
@@ -2,20 +2,27 @@ import React from "react";
 import { faLock, faUser } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, FormControl, FormGroup, InputGroup, FormLabel } from "react-bootstrap";
-
-
+import recipe from '../images/recipeGif.gif'
+import {
+    ResponsiveEmbed,
+    Button,
+} from "react-bootstrap";
 
 class Explore extends React.Component {
   render() {
     return (
-      <div id='formLogin'>    
+      <div id="formLogin">
+         {/* <div style={{ width: 660, height: "auto" }}> */}
+          <ResponsiveEmbed aspectRatio="16by9">
+            <embed type="video/mp4" src={recipe} />
+          </ResponsiveEmbed> 
+          <Button type="submit" value="Explore" size="lg" id="loginButton">
+            EXPLORE
+          </Button>
+     
       </div>
     );
   }
 }
 
 export default Explore;
-
-
-
