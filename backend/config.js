2d15788903de4173efb0b34a873ac5ac2e6435b6
Miriam <miraragal@gmail.com>
2020-12-11 16:48:40 -0700

    OAuth Facebook/Google

--- backend/config.js
+++ backend/config.js
@@ -5,7 +5,6 @@ dotenv.config();
 module.exports= {
   PORT: process.env.PORT || 3001,
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/lazyKitchen',
-  SECRET_KEY: process.env.SECRET_KEY,
   secretOrKey: process.env.secretOrKey || 'somethingsecrethere',
   facebookClientId: process.env.facebookClientId || 'facebookClientId',
   facebookClientSecret: process.env.facebookClientSecret || 'facebookClientSecret',
