89324dabdf3ce27f4082029b15364b229ac6e11f
Miriam <miraragal@gmail.com>
2020-12-14 14:33:47 -0700

    OAuth form

--- frontend/src/components/OAuth.js
+++ frontend/src/components/OAuth.js
@@ -1,93 +1,97 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    faFacebook,
-    faGoogle,
-  } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook, faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+
 
 
 export default class OAuth extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          user: {},
-          disabled: false,
-        };
-      }
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      disabled: '',
+    };
+  }
+
   componentDidMount() {
-    const { socket, provider } = this.props
-    socket.on(provider, user => {  
-      this.popup.close()
-      this.setState({user})
-    })
+    const { socket, provider } = this.props;
+      socket.on(provider, (user) => {
+        this.popup.close();
+        this.setState({ user });
+      });
   }
 
   //Re-enable the login button if the user close popup without authenticating
   checkPopup() {
     const check = setInterval(() => {
-      const { popup } = this
+      const { popup } = this;
       if (!popup || popup.closed || popup.closed === undefined) {
-        clearInterval(check)
-        this.setState({ disabled: ''})
+        clearInterval(check);
+        this.setState({ disabled: '' });
       }
-    }, 1000)
+    }, 1000);
   }
-  
+
   //Launches the popup by requesting the server and then pases along the socket id
   openPopup() {
-    const { provider, socket } = this.props
-    const width = 600, height = 600
-    const left = (window.innerWidth / 2) - (width / 2)
-    const top = (window.innerHeight / 2) - (height / 2)
-    const url = `${'http://localhost:3001'}/${provider}?socketId=${socket.id}`
+    const { provider, socket } = this.props;
+    const width = 600, height = 600;
+    const left = window.innerWidth / 2 - width / 2;
+    const top = window.innerHeight / 2 - height / 2;
+    const url = `${"http://localhost:3000"}/${provider}?socketId=${socket.id}`;
 
-    return window.open(url, '',       
+    return window.open(
+      url,
+      "",
       `toolbar=no, location=no, directories=no, status=no, menubar=no, 
       scrollbars=no, resizable=no, copyhistory=no, width=${width}, 
       height=${height}, top=${top}, left=${left}`
-    )
+    );
   }
   //Kicks off the processes of opening the popup on the server and also disables the login button if the user is already logged
   startAuth(e) {
     if (!this.state.disabled) {
-      e.preventDefault()
-      this.popup = this.openPopup()  
-      this.checkPopup()
-      this.setState({disabled: 'disabled'})
+      e.preventDefault();
+      this.popup = this.openPopup();
+      this.checkPopup();
+      this.setState({ disabled: "disabled" });
     }
   }
 
   closeCard() {
-    this.setState({user: {}})
+    this.setState({ user: {} });
   }
 
   render() {
-    const { name, photo} = this.state.user
-    const { provider } = this.props
-    const { disabled } = this.state
-    
+    const { name, photo } = this.state.user;
+    const { provider } = this.props;
+    const { disabled } = this.state;
+
     return (
       <div>
-        {name
-          ? <div className={'card'}>              
-              <img src={photo} alt={name} />
+      {name
+        ? <div>              
+            <img src={photo} alt={name} />
+            <FontAwesomeIcon
+              icon={faCircle}
+              onClick={this.closeCard.bind(this)}
+            />
+            <h4>{name}</h4>
+          </div>
+        : <div>
+            <button 
+              onClick={this.startAuth.bind(this)} 
+            >
               <FontAwesomeIcon
-                icon={'times-circle'}
-                className={'close'}
-                onClick={this.closeCard.bind(this)}
+                icon={provider}
+                size='xl'
+                id='facebookButton'
               />
-              <h4>{name}</h4>
-            </div>
-          : <div className={'button-wrapper fadein-fast'}>
-                 <FontAwesomeIcon icon={faFacebook} size="xl" id="facebookButton" onClick={this.startAuth.bind(this)} 
-                className={`${provider} ${disabled} button`}/>  
-                 <FontAwesomeIcon icon={faGoogle} size="xl" id="facebookButton" onClick={this.startAuth.bind(this)} 
-                className={`${provider} ${disabled} button`} />
-            
-            </div>
-        }
-      </div>
+            </button>
+          </div>
+      }
+    </div>
     )
   }
 }
--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -9,11 +9,11 @@ import {
   FormLabel,
 } from "react-bootstrap";
 import OAuth from "./OAuth";
-import io from "socket.io-client";
 
 //SOCIALMEDIA LOGIN
-const socket = io("http://localhost:3001");
-const providers = ["facebook", "google"];
+var io = require("socket.io-client");
+const socket = io.connect("http://localhost:3000"); //3001
+const providers = ["faFacebook", "faGoogle"];
 
 //VALIDATORS
 const required = (val) => val && val.length;
@@ -32,75 +32,72 @@ class Login extends React.Component {
     this.submitHandler = this.submitHandler.bind(this);
   }
 
-  
   submitHandler(values) {
     console.log(`Hola! ${this.username}`);
     values.preventDefault();
-   
   }
 
   render() {
-    const { authenticated } = this.state;
     return (
       <div>
-        {!authenticated ? (
-          <FormGroup
-            id="formLogin"
-            onSubmit={(values) => this.submitHandler(values)}
-          >
-            <h1>Login</h1>
-            <InputGroup>
-              <InputGroup.Prepend>
-                <FontAwesomeIcon icon={faUser} size="md" id="loginLabels" />{" "}
-                <FormLabel for="username" id="loginLabels">
-                  Username
-                </FormLabel>
-                <br />
-                <InputGroup.Text></InputGroup.Text>
-                <FormControl
-                  type="email"
-                  id="inputs"
-                  // onChange={this.stateChange(input.email)}
-                  placeholder="Type your emails as Username"
-                />
-              </InputGroup.Prepend>
+        <FormGroup
+          id="formLogin"
+          onSubmit={(values) => this.submitHandler(values)}
+        >
+          <h1>Login</h1>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <FontAwesomeIcon icon={faUser} size="md" id="loginLabels" />{" "}
+              <FormLabel for="username" id="loginLabels">
+                Username
+              </FormLabel>
               <br />
-            </InputGroup>
-            <InputGroup>
-              <InputGroup.Prepend>
-                <FontAwesomeIcon icon={faLock} size="md" id="loginLabels" />{" "}
-                <FormLabel for="username" id="loginLabels">
-                  Password
-                </FormLabel>
-                <br />
-                <InputGroup.Text></InputGroup.Text>
-                <FormControl
-                  type="text"
-                  id="inputs"
-                  placeholder="Type your password"
-                  // onChange={this.stateChange(input.email)}
-                  validators={{
-                    required,
-                    minLength: minLength(2),
-                    maxLength: maxLength(20),
-                  }}
-                />
-              </InputGroup.Prepend>
-              <small id="passwordHelp">Forgot password?</small>
-            </InputGroup>
+              <InputGroup.Text></InputGroup.Text>
+              <FormControl
+                type="email"
+                id="inputs"
+                // onChange={this.stateChange(input.email)}
+                placeholder="Type your emails as Username"
+              />
+            </InputGroup.Prepend>
             <br />
-            <Button type="submit" value="Login" size="lg" id="loginButton">
-              LOGIN
-            </Button>
-            {/* OAUTH */}
-            <p id="loginLabels">Or Sign up Using</p>
-            {providers.map(provider => {
-              <OAuth provider={provider} key={provider} socket={socket} />;
+          </InputGroup>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <FontAwesomeIcon icon={faLock} size="md" id="loginLabels" />{" "}
+              <FormLabel for="username" id="loginLabels">
+                Password
+              </FormLabel>
+              <br />
+              <InputGroup.Text></InputGroup.Text>
+              <FormControl
+                type="text"
+                id="inputs"
+                placeholder="Type your password"
+                // onChange={this.stateChange(input.email)}
+                validators={{
+                  required,
+                  minLength: minLength(2),
+                  maxLength: maxLength(20),
+                }}
+              />
+            </InputGroup.Prepend>
+            <small id="passwordHelp">Forgot password?</small>
+          </InputGroup>
+          <br />
+          <Button type="submit" value="Login" size="lg" id="loginButton">
+            LOGIN
+          </Button>
+          {/* OAUTH */}
+          <p id="loginLabels">Or Sign up Using</p>
+
+          <div>
+            {providers.map((provider) => {
+              <OAuth  socket={socket} />;
+       
             })}
-          </FormGroup>
-        ) : (
-          <h1>Welcome {this.user.name}</h1>
-        )}
+          </div>
+        </FormGroup>
       </div>
     );
   }
@@ -108,51 +105,50 @@ class Login extends React.Component {
 
 export default Login;
 
-
 // componentDidMount() {
-  //   const requestLogin = {
-  //     method: "GET",
-  //     credentials: "include",
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //       "Access-Control-Allow-Credentials": true,
-  //     },
-  //     body: JSON.stringify({ title: "React POST Request Example" }),
-  //   };
+//   const requestLogin = {
+//     method: "GET",
+//     credentials: "include",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Access-Control-Allow-Credentials": true,
+//     },
+//     body: JSON.stringify({ title: "React POST Request Example" }),
+//   };
 
-  //   fetch('"/api/users/login"', requestLogin)
-  //     .then((response) => {
-  //       if (response.status === 200) return response.json();
-  //       throw new Error("failed to authenticate user");
-  //     })
-  //     .then((data) =>
-  //       this.setState({
-  //         authenticated: true,
-  //         user: data.user,
-  //       })
-  //     )
-  //     .catch((error) => {
-  //       this.setState({
-  //         authenticated: false,
-  //         error: "Failed to authenticate user",
-  //       });
-  //     });
-  // }
+//   fetch('"/api/users/login"', requestLogin)
+//     .then((response) => {
+//       if (response.status === 200) return response.json();
+//       throw new Error("failed to authenticate user");
+//     })
+//     .then((data) =>
+//       this.setState({
+//         authenticated: true,
+//         user: data.user,
+//       })
+//     )
+//     .catch((error) => {
+//       this.setState({
+//         authenticated: false,
+//         error: "Failed to authenticate user",
+//       });
+//     });
+// }
 
-  // signin = (email, password) => async (dispatch) => {
-  //   const { userData } = await Axios.post("/api/users/login", {
-  //     email,
-  //     password,
-  //   });
-  //   fetch(userData)
-  //     .then((response) => response.json())
-  //     .then((user) => console.log("This is your user:", user))
-  //     .catch((error) => console.log(error));
-  // };
+// signin = (email, password) => async (dispatch) => {
+//   const { userData } = await Axios.post("/api/users/login", {
+//     email,
+//     password,
+//   });
+//   fetch(userData)
+//     .then((response) => response.json())
+//     .then((user) => console.log("This is your user:", user))
+//     .catch((error) => console.log(error));
+// };
 
-  // stateChange() {
-  //   this.setState({
-  //     email: input.email,
-  //     password: input.password,
-  //   });
-  // }
+// stateChange() {
+//   this.setState({
+//     email: input.email,
+//     password: input.password,
+//   });
+// }
